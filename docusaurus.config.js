c7b4dd7fbcb79c3f4ff8b48a7974eedda4c05ddb
Muhammad Swalah A A <mail@swalahamani.com>
2021-11-04 16:40:23 +0530

    chore: remove edit url for blog and docs presets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,17 +21,11 @@ const config = {
 			"@docusaurus/preset-classic",
 			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/main/website/blog/",
-				},
+				// docs: {
+				// 	sidebarPath: require.resolve("./sidebars.js"),
+				// 	// Please change this to your repo.
+				// 	editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+				// },
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
 				},
